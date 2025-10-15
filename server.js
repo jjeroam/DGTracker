878684e960bee25f3c64342441fb05f748e85ef6
jjeroam <jjjmmmdddggg@gmail.com>
2025-10-15 17:04:00 +0800

    src folder added

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
-import http from 'http';
-import fs from 'fs/promises';
-import path from 'path';
-import url from 'url';
+const express = require("express");
+const path = require("path");
 
+const app = express();
 const PORT = process.env.PORT;
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// app.use(express.static(path.join(__dirname, "public")));
 
-const server = http.createServer(async (req, res) => {
+const post = [
+  { id: 1, name: "Juan Dela Cruz", position: "Engineer" },
+  { id: 2, name: "Maria Basa", position: "Architect" },
+  { id: 3, name: "Andres Bonifacio", position: "Foreman" },
+];
 
-})
+app.get("/employees", (req, res) => {
+  const limit = parseInt(req.query.limit);
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(post.slice(0, limit));
+  }
 
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+  res.status(200).json(post);
+});
+
+app.get("/employees/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = post.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({ error: "Employee not found" });
+  }
+
+  res.status(200).json(post);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
