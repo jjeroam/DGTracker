f3e16be4b2c3fe32505291ba22595e05d5906deb
jjeroam <jjjmmmdddggg@gmail.com>
2025-10-16 18:38:11 +0800

    html structure added, CRUD operation in employeeRoutes

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-const employees = [
+let employees = [
   { id: 1, name: "Juan Dela Cruz", position: "Engineer" },
   { id: 2, name: "Maria Basa", position: "Architect" },
   { id: 3, name: "Andres Bonifacio", position: "Foreman" },
@@ -27,6 +27,49 @@ router.get("/:id", (req, res) => {
   }
 
   res.status(200).json(employee);
+});
+
+//Create new employee
+router.post("/", (req, res) => {
+  let newEmployee = {
+    id: employees.length + 1,
+    name: req.body.name,
+    position: req.body.position,
+  };
+
+  if (!newEmployee.name || !newEmployee.position) {
+    return res.status(400).json({ msg: "Please include name and position" });
+  }
+
+  employees.push(newEmployee);
+  res.status(201).json(newEmployee);
+});
+
+//Update employee by ID
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const employee = employees.find((employee) => employee.id === id);
+
+  if (!employee) {
+    return res.status(404).json({ msg: `Employee ${id} not found` });
+  }
+
+  employee.name = req.body.name;
+  employee.position = req.body.position;
+  res.status(200).json(employee);
+});
+
+//Delete employee by ID
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const employee = employees.find((employee) => employee.id === id);
+
+  if (!employee) {
+    return res.status(404).json({ msg: `Employee ${id} not found` });
+  }
+
+  employees = employees.filter((employee) => employee.id !== id);
+  res.status(200).json(employees);
 });
 
 export default router;
